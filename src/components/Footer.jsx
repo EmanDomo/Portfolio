6e0554231d4ca5d309ae7eb64f64a564530708d0
EmanDomo <100555917+EmanDomo@users.noreply.github.com>
2025-07-30 17:29:43 +0800

    minor changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="py-4 position-relative bg-dark text-light">
       <div className="dark-overlay"></div>
       <Container className="content-container">
-        <p className="text-center mb-0">© 2025 Eman Domoos. All rights reserved.</p>
+        <p className="text-center mb-0">2025 Eman Domoos. All rights reserved.</p>
       </Container>
     </footer>
   );
